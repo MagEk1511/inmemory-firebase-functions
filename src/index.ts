ca3b1d65812f82c7c1d9608b49884603bafb1602
MagEk1511 <ekamagerramov@gmail.com>
2025-03-09 02:57:40 +0300

    added few packages and created requestSchema

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,36 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import axios from "axios";
+import {z} from "zod";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+const requestSchema = z.object({
+  name: z.string().min(1, "name is required"),
+  avatarImageUrl: z.string().min(1, "imageUrl is required"),
+  coverImageUrl: z.string(),
+  voiceId: z.string().uuid(),
+  knowledge: z.object({
+    name: z.string().min(1, "knowledgeName is required"),
+    description: z.string().min(1, "knowledgeDescription is required"),
+    plaintext: z.string().min(1, "knowledgePlaintext is required"),
+  }),
+});
+
+export const createAvatar = functions.https.onRequest( async (req, res) => {
+  const parseBody = requestSchema.safeParse(req.body);
+
+  if (!parseBody.success) {
+    return res.status(400).json({
+      error: "Validation error",
+      details: parseBody.error.format(),
+    });
+  }
+
+  const {name, avatarImageUrl, coverImageUrl, voiceId, knowledge} = parseBody.data;
+);
